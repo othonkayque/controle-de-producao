781dcb5d20cf77e0726fa6a3ce44f0036ae069e2
Your Name <you@example.com>
2025-05-28 09:17:16 -0300

    refatoracao simulacao de perda

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #define TOTAL_ESPERADO 30
 int esteira1 = 0, esteira2 = 0, esteira3 = 0;
 
+int vetor[3];
+
 int calculaPerdaTotal(int valorInicial, int valorFinal) {
     int resultado = ((valorInicial - valorFinal) * 100)  / valorInicial;
     return resultado;
@@ -12,35 +14,34 @@ int calculaPerdaTotal(int valorInicial, int valorFinal) {
 
 int simulaEsteira() {
     int i = 0;
-    for(i; i<TOTAL_ESPERADO; ++i) {
+    for(i; i<TOTAL_ESPERADO; i++) {
         esteira1 = i;
+    }
+}
+
+void simulaPerda(int vetor[3]) {
+    for(int i = 0; i < 3; i++) {
+        vetor[i] = (rand() % 10) + 1;
     }
 }
 
 int main() {
     esteira1 = simulaEsteira();
-    esteira2 = esteira1;
-    esteira3 = esteira2;
+    esteira2 = simulaEsteira();
+    esteira3 = simulaEsteira();
     
-    srand(time(NULL));
-    int valor_perdido = rand() % 10 + 1;
-    esteira2 -= valor_perdido;
-    esteira3 = esteira2 - valor_perdido;
+
+    int perda[3];
+    simulaPerda(perda);
+    simulaPerda(perda);
+    simulaPerda(perda);
+    esteira1 -= perda[0];
+    esteira2 = esteira1 - perda[1];
+    esteira3 = esteira2 - perda[2];
+
     printf("esteira1: %d\nesteira2: %d\nesteira3: %d\n", esteira1, esteira2, esteira3);
-    printf("Porcentagem perdida: %d%%", calculaPerdaTotal(TOTAL_ESPERADO, esteira3));
+    printf("Porcentagem perdida: %d%%\n", calculaPerdaTotal(TOTAL_ESPERADO, esteira3));
 
-    if(esteira1 < esteira2 && esteira1 < esteira3) {
-        printf("A maioria dos produtos foram perdidos/danificados na esteira 1");
-    }
-    else if(esteira2 < esteira1 && esteira2 < esteira3) {
-        printf("A maioria dos produtos foram perdidos/danificados na esteira 2");
-    }
-    else if(esteira3 < esteira1 && esteira3 < esteira2) {
-        printf("A maioria dos produtos foram perdidos/danificados na esteira 3");
-    }
-    else {
-        printf("Não houveram perdas! :)");
-    }
-
+    system("pause");
     return 0;
 }
